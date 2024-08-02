0adf3c2d7cd58e534fe1036c003f6e09889bbe58
Vittv <vittoriobrum@hotmail.com>
2024-08-02 04:39:00 -0300

    #

--- freecodecamp/03 - Front End Development Libraries/Projects/3 - Build a Drum Machine/drum-machine/src/App.js
+++ freecodecamp/03 - Front End Development Libraries/Projects/3 - Build a Drum Machine/drum-machine/src/App.js
@@ -68,6 +68,7 @@ function App() {
   return (
     <div className="App">
       <div id="drum-machine">
+        <h3 id="title">DRUM MACHINE</h3>
         <div id="display">{activeKey}</div>
         <div className="drum-pads">
           {drumPads.map((drumPad) => (
