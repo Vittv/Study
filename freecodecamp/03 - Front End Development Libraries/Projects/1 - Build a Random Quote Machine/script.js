aad409620c7bb3518c25086bf207259772ca83e2
Vittv <166668150+Vittv@users.noreply.github.com>
2025-01-30 20:36:41 -0300

    #

--- freecodecamp/03 - Front End Development Libraries/Projects/1 - Build a Random Quote Machine/script.js
+++ freecodecamp/03 - Front End Development Libraries/Projects/1 - Build a Random Quote Machine/script.js
@@ -9,18 +9,34 @@ $(document).ready(function() {
 
     function displayQuote() {
         getRandomQuote().done(function(data) {
-            $('#text').text(data.content);
-            $('#author').text(`${data.author}`);
+            $('#text').fadeOut(500, function() {
+                $(this).text(data.content).fadeIn(500);
+            })
+            $('#author').fadeOut(500, function() {
+                $(this).text(`${data.author}`).fadeIn(500);
+            });
+
+            console.log(`${data.content}`)
             updateTweetButton(data.content, data.author);
+            updateTumblrButton(data.content, data.author);
         }).fail(function() {
-            $('#text').text("An error occurred. Please try again.");
-            $('#author').text("");
+            $('#text').fadeOut(500, function() {
+                $(this).text("An error occurred. Please try again.").fadeIn(500);
+            });
+            $('#author').fadeOut(500, function() {
+                $(this).text("").fadeIn(500);
+            });
         });
     }
 
     function updateTweetButton(quote, author) {
         const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(`"${quote}" - ${author}`)}`;
         $('#tweet-quote').attr('href', tweetUrl);
+    }
+
+    function updateTumblrButton(quote, author) {
+        const tumblrUrl = `https://www.tumblr.com/share/link?url=${encodeURIComponent(`"${quote}" - ${author}`)}`;
+        $('#tumblr-quote').attr('href', tumblrUrl);
     }
 
     $('#new-quote').click(function() {
